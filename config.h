e94ea80ba5ea1733caee1c844250e0e15fafbdd8
VDreamerlu96 <131884769+VDreamerlu96@users.noreply.github.com>
2025-04-02 18:47:37 +0800

    Update config.h

--- config.h
+++ config.h
@@ -7,3 +7,21 @@
 #define Board_Start_Height 20
 
 #define Dwm_Num 7
+
+/*
+//这是B版本的config，要把上面注释并将这个注释取消
+#pragma once
+
+#define Width_Cut_Num 8      //宽度除数
+#define Left_Board_Num 3     //左宽度倍数
+#define Right_Board_Num 3    //右边宽度倍数
+//更改的话要自己调补偿，我没实现算法,一般除数是偶数都没问题
+#define Height_Cut_Num 3
+#define Down_Board_Num 1
+
+#define Dwm_Num 7
+
+//文字相对位置在draw_button
+
+#define Scroo_Mouse_Count 2
+*/
